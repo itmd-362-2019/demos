4d70b2a8fcb7043527b0a710f5ecc215ab580ff7
Karl Stolley <karl.stolley@gmail.com>
2019-02-04 16:09:00 -0600

    Add basic JavaScript and load into HTML

---
+++ site.js
@@ -0,0 +1,14 @@
+(function(){
+  // Browser sanity check:
+  if (!('querySelector' in document && 'addEventListener' in document)) {
+    // Old, old browser. Say buh-bye
+    console.log('Old browser');
+    return;
+  }
+
+  document.addEventListener('DOMContentLoaded',function(){
+    console.log('OMG the DOM is loaded!!!1!');
+    var heading_text = document.querySelector('#content h1').innerText;
+    console.log('The heading text is:', heading_text);
+  });
+}());
