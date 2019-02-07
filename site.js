205381a22cb6b56285d0d478a126b50beefe28d6
Karl Stolley <karl.stolley@gmail.com>
2019-02-06 19:41:55 -0600

    Add submit-button disabled state CSS and JS

--- site.js
+++ site.js
@@ -6,10 +6,15 @@
     return;
   }
 
+  var phone_submit = document.querySelector('#signup');
+
   document.addEventListener('DOMContentLoaded',function(){
     // console.log('OMG the DOM is loaded!!!1!');
     // var heading_text = document.querySelector('#content h1').innerText;
     // console.log('The heading text is:', heading_text);
+    // Disable the submit button until we are reasonable sure
+    // that we have a ten-digit phone number
+    phone_submit.setAttribute('disabled','disabled');
   });
 
   var tel_input = document.querySelector('#telephone');
