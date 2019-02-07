2a54a050f14c3c0b174b6bbe47dbea3e214c33b3
Karl Stolley <karl.stolley@gmail.com>
2019-02-06 19:40:53 -0600

    Add input event listeners and logs

--- site.js
+++ site.js
@@ -11,4 +11,16 @@
     // var heading_text = document.querySelector('#content h1').innerText;
     // console.log('The heading text is:', heading_text);
   });
+
+  var tel_input = document.querySelector('#telephone');
+  tel_input.addEventListener('focus', function(){
+    console.log('OMG somebody focused on the telephone input');
+  });
+  tel_input.addEventListener('blur', function(){
+    console.log('OMG somebody navigated away from the telephone input');
+  tel_input.addEventListener('keyup',function(){
+    console.log('The value of #telephone is', this.value);
+  });
+
+
 }());
