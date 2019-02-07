d2fe3c07598275420efca529ca117ec89d86ce76
Karl Stolley <karl.stolley@gmail.com>
2019-02-06 19:44:50 -0600

    Add logic to sanitize number and make form submittable

--- site.js
+++ site.js
@@ -23,8 +23,20 @@
   });
   tel_input.addEventListener('blur', function(){
     console.log('OMG somebody navigated away from the telephone input');
+  });
+
   tel_input.addEventListener('keyup',function(){
-    console.log('The value of #telephone is', this.value);
+    // Remove all non-digit characters from the telephone input's value
+    var clean_number = this.value.replace(/\D/g,'');
+    // Remove any 1 apppearing at the start of the number
+    var sanitized_number = clean_number.replace(/^1/,'');
+    // Finally, check to see if the number is 10 digits long
+    if (sanitized_number.length === 10) {
+      console.log('That number looks great!');
+      // If so, allow the form to be submitted
+      phone_submit.removeAttribute('disabled');
+    }
+
   });
 
 
