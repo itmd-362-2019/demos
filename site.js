80d5f660648bb978eb6de09e02b0028268a8861a
Karl Stolley <karl.stolley@gmail.com>
2019-02-06 19:38:07 -0600

    Comment out old diagnostic JavaScript

--- site.js
+++ site.js
@@ -7,8 +7,8 @@
   }
 
   document.addEventListener('DOMContentLoaded',function(){
-    console.log('OMG the DOM is loaded!!!1!');
-    var heading_text = document.querySelector('#content h1').innerText;
-    console.log('The heading text is:', heading_text);
+    // console.log('OMG the DOM is loaded!!!1!');
+    // var heading_text = document.querySelector('#content h1').innerText;
+    // console.log('The heading text is:', heading_text);
   });
 }());
