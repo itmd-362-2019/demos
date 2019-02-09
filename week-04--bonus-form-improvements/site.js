d3bcb2c63c5571819a5670b63d280b07ac6c82c6
Karl Stolley <karl.stolley@gmail.com>
2019-02-09 17:07:18 -0500

    Add library of resuable data-checking functions

--- week-04--bonus-form-improvements/site.js
+++ week-04--bonus-form-improvements/site.js
@@ -6,6 +6,74 @@
     return;
   }
 
+  // Library of comparison functions
+  //
+  // Unlike the raw operators these encapsulate, functions
+  // can be passed around like any other value into other
+  // functions.
+  function eq(value,condition) {
+    return value === condition;
+  }
+  function gt(value,condition) {
+    return value > condition;
+  }
+  function gte(value,condition) {
+    return value >= condition;
+  }
+  function lt(value,condition) {
+    return value < condition;
+  }
+  function lte(value,condition) {
+    return value <= condition;
+  }
+
+  // Data cleanup functions
+  function clean_nonnumbers(value) {
+    // returns value with all non-digits removed
+    return value.replace(/\D/g,'');
+  }
+  function clean_whitespace(value) {
+    // returns value with all whitespace characters removed
+    return value.replace(/\s/g, '');
+  }
+
+  // Phone-specific santizier functions
+  function strip_us_country_code(value) {
+    return value.replace(/^1/,'');
+  }
+
+  // All purpose validate function. It takes a value,
+  // along with either a regular expression pattern or
+  // a simple function -- like the comparison functions
+  // above -- and a condition. JavaScript doesn't char
+  // if a function is called with more or fewer arguments
+  // than described in the function definition, so it's
+  // no problem at all to leave off the `condition`
+  // argument when calling a check that's a regular expression
+  function validate(value,check,condition) {
+    if (eq(typeof(check.test),'function')) {
+      // Handle a regular expression
+      return check.test(value);
+    } else if (eq(typeof(check),'function')) {
+      // Handle a comparison function
+      return check(value,condition);
+    } else {
+      return false;
+    }
+  }
+
+  // Phone validity functions
+  function validate_us_phone(value) {
+    var phone_number = strip_us_country_code(clean_nonnumbers(value));
+    return validate(phone_number.length,eq,10);
+  }
+
+  // Email validity function
+  function validate_email(value) {
+    var email = clean_whitespace(value);
+    return validate(email,/^[^@\s]+@[^@\s]+$/g);
+  }
+
   var phone_submit = document.querySelector('#signup');
 
   document.addEventListener('DOMContentLoaded',function(){
