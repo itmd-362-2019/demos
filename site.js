37a54a086f7a1ffd569c4c1b47d045cef84c97db
Karl Stolley <karl.stolley@gmail.com>
2019-02-20 15:31:17 -0600

    Bring over bonus, improved site JavaScript

--- site.js
+++ site.js
@@ -6,38 +6,99 @@
     return;
   }
 
-  var phone_submit = document.querySelector('#signup');
+  // Library of comparison functions
+  //
+  // Unlike the raw operators these encapsulate, functions
+  // can be passed around like any other value into other
+  // functions.
+  function eq(value,condition) {
+    return value === condition;
+  }
+  function gt(value,condition) {
+    return value > condition;
+  }
+  function gte(value,condition) {
+    return value >= condition;
+  }
+  function lt(value,condition) {
+    return value < condition;
+  }
+  function lte(value,condition) {
+    return value <= condition;
+  }
+
+  // Data cleanup functions
+  function clean_nonnumbers(value) {
+    // returns value with all non-digits removed
+    return value.replace(/\D/g,'');
+  }
+  function clean_whitespace(value) {
+    // returns value with all whitespace characters removed
+    return value.replace(/\s/g, '');
+  }
+
+  // Phone-specific santizier functions
+  function strip_us_country_code(value) {
+    return value.replace(/^1/,'');
+  }
+
+  // All purpose validate function. It takes a value,
+  // along with either a regular expression pattern or
+  // a simple function -- like the comparison functions
+  // above -- and a condition. JavaScript doesn't char
+  // if a function is called with more or fewer arguments
+  // than described in the function definition, so it's
+  // no problem at all to leave off the `condition`
+  // argument when calling a check that's a regular expression
+  function validate(value,check,condition) {
+    if (eq(typeof(check.test),'function')) {
+      // Handle a regular expression
+      return check.test(value);
+    } else if (eq(typeof(check),'function')) {
+      // Handle a comparison function
+      return check(value,condition);
+    } else {
+      return false;
+    }
+  }
+
+  // Phone validity functions
+  function validate_us_phone(value) {
+    var phone_number = strip_us_country_code(clean_nonnumbers(value));
+    return validate(phone_number.length,eq,10);
+  }
+
+  // Email validity function
+  function validate_email(value) {
+    var email = clean_whitespace(value);
+    return validate(email,/^[^@\s]+@[^@\s]+$/g);
+  }
+
 
   document.addEventListener('DOMContentLoaded',function(){
-    // console.log('OMG the DOM is loaded!!!1!');
-    // var heading_text = document.querySelector('#content h1').innerText;
-    // console.log('The heading text is:', heading_text);
+    // Select the necessary elements from the DOM
+    var signup_form = document.querySelector('#emergencyalerts')
+    var signup_submit = document.querySelector('#signup');
+    var email_input = document.querySelector('#email');
+    var tel_input = document.querySelector('#telephone');
     // Disable the submit button until we are reasonable sure
     // that we have a ten-digit phone number
-    phone_submit.setAttribute('disabled','disabled');
+    signup_submit.setAttribute('disabled','disabled');
+
+    // Listen for keyup event ANYWHERE in the form
+    signup_form.addEventListener('keyup',function(){
+      // Check the likely validity of phone AND email
+      if (validate_us_phone(tel_input.value) && validate_email(email_input.value)) {
+        // If both are valid, remove the disabled attribute on the submit button
+        signup_submit.removeAttribute('disabled');
+      } else {
+        // This will re-disable the submit button if the input changes to an invalid state
+        signup_submit.setAttribute('disabled','disabled');
+      }
+    });
+
+  // End of DOMContentLoaded
   });
 
-  var tel_input = document.querySelector('#telephone');
-  tel_input.addEventListener('focus', function(){
-    console.log('OMG somebody focused on the telephone input');
-  });
-  tel_input.addEventListener('blur', function(){
-    console.log('OMG somebody navigated away from the telephone input');
-  });
-
-  tel_input.addEventListener('keyup',function(){
-    // Remove all non-digit characters from the telephone input's value
-    var clean_number = this.value.replace(/\D/g,'');
-    // Remove any 1 apppearing at the start of the number
-    var sanitized_number = clean_number.replace(/^1/,'');
-    // Finally, check to see if the number is 10 digits long
-    if (sanitized_number.length === 10) {
-      console.log('That number looks great!');
-      // If so, allow the form to be submitted
-      phone_submit.removeAttribute('disabled');
-    }
-
-  });
-
-
+// End of IIFE
 }());
